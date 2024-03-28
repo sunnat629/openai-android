75f7bfae49ce3e162a6a31e4e805bd649701e626
S M Mohi Us Sunnat <suncha629@gmail.com>
2024-03-28 08:58:07 +0100

    Added request and response models for Embeddings

--- openai-android/src/main/java/dev/sunnat629/openai_android/apis/embeddings/EmbeddingsRepository.kt
+++ openai-android/src/main/java/dev/sunnat629/openai_android/apis/embeddings/EmbeddingsRepository.kt
@@ -1,18 +1,31 @@
 package dev.sunnat629.openai_android.apis.embeddings
 
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.client.statement.HttpResponse
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.util.InternalAPI
 
 data class MockResponse(val result: String)
 
 interface EmbeddingsRepository {
 
     // Embeddings Operations
-    suspend fun createEmbedding(): MockResponse
+    suspend fun createEmbedding(request: CreateEmbeddingsRequest): CreateEmbeddingsResponse
 }
 
 class EmbeddingsRepositoryImpl(private val client: HttpClient): EmbeddingsRepository {
     private val baseUrl = "https://api.openai.com/v1/embeddings"
 
-    override suspend fun createEmbedding(): MockResponse = client.post(baseUrl) { /* Request body */ }
+    override suspend fun createEmbedding(request: CreateEmbeddingsRequest): CreateEmbeddingsResponse {
+        val response: HttpResponse = client.post(baseUrl) {
+            contentType(ContentType.Application.Json)
+            setBody(request)
+        }
+        // Explicitly specify the expected response type
+        return response.body<CreateEmbeddingsResponse>()
+    }
 }
