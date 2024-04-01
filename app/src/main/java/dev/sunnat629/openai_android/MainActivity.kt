089b5b1a3bc5d26cecd1422a0cc5eb20e7360b67
S M Mohi Us Sunnat <sunnat629@users.noreply.github.com>
2024-04-01 14:07:09 +0200

    App layer try (#3)
    * model repo can be fetched from OpenAi lib

--- app/src/main/java/dev/sunnat629/openai_android/MainActivity.kt
+++ app/src/main/java/dev/sunnat629/openai_android/MainActivity.kt
@@ -1,6 +1,7 @@
 package dev.sunnat629.openai_android
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,13 +9,22 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import dev.sunnat629.openai_android.networks.onFailure
+import dev.sunnat629.openai_android.networks.onSuccess
 import dev.sunnat629.openai_android.ui.theme.OpenAiAndroidTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val openAI = OpenAIBuilder(BuildConfig.OPEN_AI_TOKEN)
+            .build()
+
         setContent {
             OpenAiAndroidTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,6 +32,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    LaunchedEffect(Unit) {
+                        lifecycleScope.launch {
+                            openAI.retrieveModel("dall-e-2")
+                                .onSuccess { Log.d("ASDF", it.toString()) }
+                                .onFailure { Log.e("ASDF", it.toString()) }
+                        }
+                    }
                     Greeting("Android")
                 }
             }
