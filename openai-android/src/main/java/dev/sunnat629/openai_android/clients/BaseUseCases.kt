61f82e2dcc29be4d99edaa74a8512653f0e0b8f5
S M Mohi Us Sunnat <suncha629@gmail.com>
2024-04-02 14:26:28 +0200

    Chat api now ready to use from client side

--- openai-android/src/main/java/dev/sunnat629/openai_android/clients/BaseUseCases.kt
+++ openai-android/src/main/java/dev/sunnat629/openai_android/clients/BaseUseCases.kt
@@ -1,9 +1,5 @@
 package dev.sunnat629.openai_android.clients
 
-import dev.sunnat629.openai_android.models.chats.ChatResponse
-import dev.sunnat629.openai_android.networks.ApiResult
-
-
 interface BaseUseCases {
 
     fun model(model: String): BaseUseCases
