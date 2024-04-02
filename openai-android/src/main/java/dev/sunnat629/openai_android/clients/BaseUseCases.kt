a7ed208c700197e25b35a78302ce09d4bb2b06d9
S M Mohi Us Sunnat <suncha629@gmail.com>
2024-04-02 14:30:32 +0200

    Added base usecases for all client api

--- openai-android/src/main/java/dev/sunnat629/openai_android/clients/BaseUseCases.kt
+++ openai-android/src/main/java/dev/sunnat629/openai_android/clients/BaseUseCases.kt
@@ -1,4 +1,13 @@
 package dev.sunnat629.openai_android.clients
 
-class BaseUsecase {
+import dev.sunnat629.openai_android.models.chats.ChatResponse
+import dev.sunnat629.openai_android.networks.ApiResult
+
+
+interface BaseUseCases {
+
+    fun model(model: String): BaseUseCases
+    fun role(role: String): BaseUseCases
+    fun text(text: String): BaseUseCases
+    fun imageUrl(imageUrl: String): BaseUseCases
 }
